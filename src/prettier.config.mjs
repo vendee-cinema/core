91374d6f5d0d98ad7ff3e81f3e0595fca44dc712
bagdenius <bagdensparrow@gmail.com>
2026-03-09 17:26:03 +0200

    change print width to 80

--- src/prettier.config.mjs
+++ src/prettier.config.mjs
@@ -1,21 +1,17 @@
 /** @type {import("prettier").Config} */
 export default {
-  trailingComma: "none",
-  tabWidth: 2,
-  printWidth: 100,
-  useTabs: true,
-  semi: false,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  arrowParens: "avoid",
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  importOrderCaseInsensitive: true,
-  importOrderParserPlugins: [
-    "classProperties",
-    "decorators-legacy",
-    "typescript",
-  ],
-  importOrder: ["<THIRD_PARTY_MODULES>", "^@/", "^../", "^./"],
-  plugins: ["@trivago/prettier-plugin-sort-imports"],
-};
+	trailingComma: 'none',
+	tabWidth: 2,
+	printWidth: 80,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	arrowParens: 'avoid',
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	importOrderCaseInsensitive: true,
+	importOrderParserPlugins: ['classProperties', 'decorators-legacy', 'typescript'],
+	importOrder: ['<THIRD_PARTY_MODULES>', '^@/', '^../', '^./'],
+	plugins: ['@trivago/prettier-plugin-sort-imports']
+}
